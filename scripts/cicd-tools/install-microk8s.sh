896d96de3a3da7d0951c4b390789761594b1c198
Carmine Rimi <carmine.rimi@gmail.com>
2019-02-27 01:51:44 +1100

    Add install scripts for cicd tool-chain
    Adding scripts for gitlab, helm, nexus, spinnaker.
    
    nexus and spinnaker are just stubs.

--- scripts/cicd-tools/install-microk8s.sh
+++ scripts/cicd-tools/install-microk8s.sh
@@ -13,15 +13,34 @@ SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 # Otherwise, install from the Internet.
 function install_kubernetes() {
   install_snap microk8s --stable --classic
+
+  # export the kubectl config file in case other tools rely on this
+  mkdir -p $HOME/.kube
+  microk8s.kubectl config view --raw > $HOME/.kube/config
+  echo "Waiting for kubernetes core services to be ready.."
+  microk8s.status --wait-ready
+  sudo snap alias microk8s.kubectl kubectl
+}
+
+function install_addons() {
+  microk8s.enable dns
+  microk8s.enable dashboard
+  microk8s.enable ingress
+  microk8s.enable storage
+
   # Ensure kubernetes is running properly
-  echo "Checking kube-system status until valid response.."
-  sleep 6 ## TODO: better way to test the port is up..
-  until [[ `kubectl get pods -n=kube-system -o jsonpath='{.items}'| grep '\[\]'` == "[]" ]] ; do
-    echo ".. Sleeping 3 seconds"
+  echo "Checking kubernetes addons are running.."
+
+  until [[ `kubectl get pods --all-namespaces | grep -o 'ContainerCreating' | wc -l` == 0 ]] ; do
+    echo "Waiting for kubernetes addon service pods to be ready, sleeping 10s  ("`kubectl get pods --all-namespaces | grep -o 'ContainerCreating' | wc -l`" not running)"
+    sleep 10
   done
 }
 
+##
 # Ensure KUBECONFIG is in .bashrc so that other kubernetes tools can use it
+# NB: No longer needed (ensuring default lcoation ~/.kube/config instaed).. not used
+#
 function ensure_kubeconfig() {
   grep KUBECONFIG ${HOME}/.bashrc > /dev/null 2>&1
   if [ $? -eq 0 ]; then
@@ -34,9 +53,7 @@ function ensure_kubeconfig() {
 
 ensure_root
 install_kubernetes
-microk8s.enable dns
-microk8s.enable dashboard
-# microk8s.enable storage
-ensure_kubeconfig
-# This gets around an open issue with all-in-one installs
-iptables -P FORWARD ACCEPT
+install_addons
+
+microk8s.kubectl config view --raw > $HOME/.kube/config
+sudo snap alias microk8s.kubectl kubectl
